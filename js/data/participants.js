77e2888d8717b005368255a04f5daf23f233e30d
Kenn Thompson <kennt@shelbyinc.com>
2020-07-29 08:42:09 -0500

    Replacie i++ with static id.

--- js/data/participants.js
+++ js/data/participants.js
@@ -1,25 +1,20 @@
-var i = 1;
 
 const participants = [
-    { id: i++, name: "Wright, Nitzi", departmentId: 4, meetings: [1, 2, 3] },
-    { id: i++, name: "Flippen, Josh", departmentId: 4, meetings: [1, 2, 3] },
-
-    { id: i++, name: "Ayer, Jason", departmentId: 3, meetings: [1, 2] },
-    { id: i++, name: "Hodge, Hannah", departmentId: 3, meetings: [1, 2] },
-
-    { id: i++, name: "Canfield, Brooks", departmentId: 3, meetings: [1, 2] },
-    { id: i++, name: "Maddox, Jeff", departmentId: 3, meetings: [1, 2] },
-    { id: i++, name: "Studdard, David", departmentId: 3, meetings: [1, 2] },
-    { id: i++, name: "Thompson, Kenn", departmentId: 3, meetings: [1, 2] },
-
-    { id: i++, name: "Eoff, Edward", departmentId: 6, meetings: [1, 2] },
-
-    { id: i++, name: "Rhodes, Matt", departmentId: 5, meetings: [2] },
-    { id: i++, name: "Carter, Jerry", departmentId: 5, meetings: [2] },
-
-    { id: i++, name: "Canady, Frank", departmentId: 3, meetings: [1, 2] },
-    { id: i++, name: "Johnson, Alfred", departmentId: 2, meetings: [1, 2] },
-    { id: i++, name: "Anthony, Richard", departmentId: 1, meetings: [1, 2] },
+    { id: 1, name: "Anthony, Richard", departmentId: 1, role: 1, meetings: [1, 2] },
+    { id: 2, name: "Johnson, Alfred", departmentId: 2, role: 1, meetings: [1, 2] },
+    { id: 3, name: "Canady, Frank", departmentId: 3, role: 1, meetings: [1, 2] },
+    { id: 4, name: "Wright, Nitzi", departmentId: 4, role: 6, meetings: [1, 2, 3] },
+    { id: 5, name: "Flippen, Josh", departmentId: 4, role: 6, meetings: [1, 2, 3] },
+    { id: 6, name: "Ayer, Jason", departmentId: 3, role: 5, meetings: [1, 2] },
+    { id: 7, name: "Hodge, Hannah", departmentId: 3, role: 5, meetings: [1, 2] },
+    { id: 8, name: "Canfield, Brooks", departmentId: 3, role: 4, meetings: [1, 2] },
+    { id: 9, name: "Maddox, Jeff", departmentId: 3, role: 4, meetings: [1, 2] },
+    { id: 10, name: "Studdard, David", departmentId: 3, role: 4, meetings: [1, 2] },
+    { id: 11, name: "Thompson, Kenn E.", departmentId: 3, role: 4, meetings: [1, 2] },
+    { id: 12, name: "Wilson, Tim", departmentId: 3, role: 4, meetings: [1, 2] },
+    { id: 13, name: "Eoff, Edward", departmentId: 6, role: 3, meetings: [2] },
+    { id: 14, name: "Rhodes, Matt", departmentId: 5, role: 2, meetings: [2] },
+    { id: 15, name: "Carter, Jerry", departmentId: 5, role: 2, meetings: [2] },
 ];
 
 class Repo {
