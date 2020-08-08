443d83df354c70286b0c6ad004d266bc4e693d0e
david.studdard@shelbysystems.onmicrosoft.com <david.studdard@shelbysystems.onmicrosoft.com>
2020-08-08 11:05:46 -0500

    Resetting meeting times
    putting a little more space under the progress bar

--- js/data/meetings.js
+++ js/data/meetings.js
@@ -1,7 +1,9 @@
 const Type = {
+    None: 0,
     Scrum: 1,
     GoNoGo: 2,
-    Review: 3
+    SprintReview: 3,
+    SprintPlanning: 4
 };
 
 const meetings = [
@@ -9,21 +11,21 @@ const meetings = [
         id: Type.Scrum,
         title: "Daily Stand-Up Meeting",
         description: "Scrum Daily Standup",
-        time: { start: "22:25", duration: "5" },
+        time: { start: "8:45", duration: "15" },
         imageUrl: "/img/splash.png"
     },
     {
         id: Type.GoNoGo,
         title: "Go/No-Go",
         description: "",
-        time: { start: "22:30", duration: "30" },
+        time: { start: "14:00", duration: "30" },
         imageUrl: ""
     },
     {
-        id: Type.Review,
+        id: Type.SprintReview,
         title: "Review",
         description: "",
-        time: { start: "08:45", duration: "15" },
+        time: { start: "09:00", duration: "60" },
         imageUrl: ""
     }
 ];
