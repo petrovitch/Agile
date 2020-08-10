6d905738db34e60c45a871d27de279666187de3e
david.studdard@shelbysystems.onmicrosoft.com <david.studdard@shelbysystems.onmicrosoft.com>
2020-08-10 09:12:01 -0500

    Attendees: Started controller Added meeting roles

---
+++ js/controllers/attendees.js
@@ -0,0 +1,7 @@
+class Attendees {
+
+}
+
+export default Attendees {
+
+}
--- js/data/attendees.js
+++ js/data/attendees.js
@@ -1,20 +1,25 @@
+const Type = {
+    None: 0,
+    Lead: 1
+}
+
 const attendees = [
-    { id: 1, name: { last: "Anthony", first: "Richard" }, departmentId: 1, meetings: [1, 2] },
-    { id: 2, name: { last: "Johnson", first: "Alfred" }, departmentId: 2, meetings: [1, 2] },
-    { id: 3, name: { last: "Canady", first: "Frank" }, departmentId: 3, meetings: [1, 2] },
-    { id: 4, name: { last: "Wright", first: "Nitzi" }, departmentId: 4, meetings: [1, 2, 3] },
-    { id: 5, name: { last: "Flippen", first: "Josh" }, departmentId: 4, meetings: [1, 2, 3] },
-    { id: 6, name: { last: "Ayer", first: "Jason" }, departmentId: 3, meetings: [1, 2] },
-    { id: 7, name: { last: "Hodge", first: "Hannah" }, departmentId: 3, meetings: [1, 2] },
-    { id: 8, name: { last: "Canfield", first: "Brooks" }, departmentId: 3, meetings: [1, 2] },
-    { id: 9, name: { last: "Maddox", first: "Jeff" }, departmentId: 3, meetings: [1, 2] },
-    { id: 10, name: { last: "Studdard", first: "David" }, departmentId: 3, meetings: [1, 2] },
-    { id: 11, name: { last: "Thompson", first: "Kenn E." }, departmentId: 3, meetings: [1, 2] },
-    { id: 12, name: { last: "Wilson", first: "Tim" }, departmentId: 3, meetings: [1, 2] },
-    { id: 13, name: { last: "Eoff", first: "Edward" }, departmentId: 6, meetings: [2] },
-    { id: 14, name: { last: "Rhodes", first: "Matt" }, departmentId: 5, meetings: [2] },
-    { id: 15, name: { last: "Carter", first: "Jerry" }, departmentId: 5, meetings: [2] },
-    { id: 16, name: { last: "Benson", first: "Christopher" }, departmentId: 6, meetings: [2] },
+    { id: 1, name: { last: "Anthony", first: "Richard" }, departmentId: 1, role: Type.Lead, meetings: [1, 2] },
+    { id: 2, name: { last: "Johnson", first: "Alfred" }, departmentId: 2, role: Type.Lead, meetings: [1, 2] },
+    { id: 3, name: { last: "Canady", first: "Frank" }, departmentId: 3, role: Type.None, meetings: [1, 2] },
+    { id: 4, name: { last: "Wright", first: "Nitzi" }, departmentId: 4, role: Type.None, meetings: [1, 2, 3] },
+    { id: 5, name: { last: "Flippen", first: "Josh" }, departmentId: 4, role: Type.None, meetings: [1, 2, 3] },
+    { id: 6, name: { last: "Ayer", first: "Jason" }, departmentId: 3, role: Type.None, meetings: [1, 2] },
+    { id: 7, name: { last: "Hodge", first: "Hannah" }, departmentId: 3, role: Type.None, meetings: [1, 2] },
+    { id: 8, name: { last: "Canfield", first: "Brooks" }, departmentId: 3, role: Type.None, meetings: [1, 2] },
+    { id: 9, name: { last: "Maddox", first: "Jeff" }, departmentId: 3, role: Type.None, meetings: [1, 2] },
+    { id: 10, name: { last: "Studdard", first: "David" }, departmentId: 3, role: Type.None, meetings: [1, 2] },
+    { id: 11, name: { last: "Thompson", first: "Kenn E." }, departmentId: 3, role: Type.None, meetings: [1, 2] },
+    { id: 12, name: { last: "Wilson", first: "Tim" }, departmentId: 3, role: Type.None, meetings: [1, 2] },
+    { id: 13, name: { last: "Eoff", first: "Edward" }, departmentId: 6, role: Type.None, meetings: [2] },
+    { id: 14, name: { last: "Rhodes", first: "Matt" }, departmentId: 5, role: Type.None, meetings: [2] },
+    { id: 15, name: { last: "Carter", first: "Jerry" }, departmentId: 5, role: Type.None, meetings: [2] },
+    { id: 16, name: { last: "Benson", first: "Christopher" }, departmentId: 6, role: Type.None, meetings: [2] },
 ];
 
 class Repo {
