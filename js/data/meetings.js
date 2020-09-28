d3504fb4e4aca5342bd874fe706e39d589de8c67
Kenn Thompson <kennt@shelbyinc.com>
2020-09-28 07:51:05 -0500

    Daily Standup moved to 8:45

--- js/data/meetings.js
+++ js/data/meetings.js
@@ -2,11 +2,40 @@ import { RoleType as Role } from './types.js';
 
 const meetings = [
     {
+        id: 3,
+        title: "Product Planning",
+        time: { start: "10:00", duration: "240" },
+        imageUrl: ""
+    },
+    {
         id: 1,
-        title: "Daily Stand-Up Meeting",
-        time: { start: "9:00", duration: "15" },
+        title: "Daily Scrum Meeting",
+        time: { start: "8:45", duration: "15" },
         imageUrl: "/img/splash.png"
     },
+    {
+        id: 4,
+        title: "Daily Meeting with Product Owner and Management",
+        time: { start: "9:00", duration: "5" },
+        imageUrl: ""
+    },
+    {
+        id: 5,
+        title: "Scrum Review",
+        time: { start: "14:00", duration: "240" },
+        imageUrl: ""
+    },
+    {
+        id: 6,
+        title: "Scrum Retrospective",
+        time: { start: "14:00", duration: "180" },
+        imageUrl: ""
+    },
+    {
+        id: 7,
+        title: "Product Backlog Refinement",
+        time: { start: "14:00", duration: "180" },
+        imageUrl: ""
     {
         id: 2,
         title: "Go/No-Go",
