907d6dfd7526a53df87fa61e3bca1088f214b5ef
Kenn Thompson <kennt@shelbyinc.com>
2020-09-28 12:05:52 -0500

    Changes to Scrum Events and other meetings.

--- js/data/meetings.js
+++ js/data/meetings.js
@@ -2,42 +2,65 @@ import { RoleType as Role } from './types.js';
 
 const meetings = [
     {
+        // Sprint Planning is time-boxed to a maximum of eight hours for a one-month Sprint.
+        // For shorter Sprints, the event is usually shorter.
         id: 3,
-        title: "Product Planning",
+        title: "Sprint Planning",
         time: { start: "10:00", duration: "240" },
         imageUrl: ""
     },
     {
+        // The Daily Scrum is a 15-minute time-boxed event for the Development Team.
         id: 1,
         title: "Daily Scrum Meeting",
         time: { start: "8:45", duration: "15" },
         imageUrl: "/img/splash.png"
     },
     {
+        // This is not a Scrum Event.
         id: 4,
         title: "Daily Meeting with Product Owner and Management",
         time: { start: "9:00", duration: "5" },
         imageUrl: ""
     },
     {
+        // The Develoment Team or team members often meet immediately after the 
+        // Daily Scrum for detailed discussions, or to adapt, or replan, the rest 
+        // of the Sprint's work.
+        id: 8,
+        title: "Detailed Discussions",
+        time: { start: "9:00", duration: "15" },
+        imageUrl: ""
+    },
+    {
+        // This is at most a four-hour meeting for one-month Sprints.  For shorter
+        // Sprints, the event is usually shorter.  Attendees include the Scrum Team
+        // (Product Owner, Scrum Master, and Development Team) and key stakeholders 
+        // invited by the Product Owner.
         id: 5,
-        title: "Scrum Review",
+        title: "Sprint Review",
         time: { start: "14:00", duration: "240" },
         imageUrl: ""
     },
     {
+        // This is at most a three-hour meeting for one-month Sprints.  For shorter
+        // Sprints, the even is usually shorter. The Scrum Master participates as a 
+        // peer team member in the meeting from the accountability over the Scrum
+        // process.  It is an opportunity for the Scrum Team to inspect itself.
         id: 6,
-        title: "Scrum Retrospective",
+        title: "Sprint Retrospective",
         time: { start: "14:00", duration: "180" },
         imageUrl: ""
     },
     {
+        // This is not a Scrum Event.
         id: 7,
         title: "Product Backlog Refinement",
         time: { start: "14:00", duration: "180" },
         imageUrl: ""
     },
     {
+        // This is not a Scrum Event.
         id: 2,
         title: "Go/No-Go",
         time: { start: "14:00", duration: "30" },
